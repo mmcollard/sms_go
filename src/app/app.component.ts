6c9a093574bf94b43e9226199f10d652e93b0a10
Paloma Rivas <privas@smsargentina.com.ar>
2024-03-14 14:41:34 -0300

    unificación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BotonesComponent } from './layout/botones/botones.component';
 import { CardPublicacionComponent } from "./layout/card-publicacion/card-publicacion.component";
+import { BarraLateralComponent } from "./layout/barra-lateral/barra-lateral.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, BotonesComponent, CardPublicacionComponent]
+    imports: [RouterOutlet, BotonesComponent, BarraLateralComponent, CardPublicacionComponent]
 })
 export class AppComponent {
   title = 'proyectoangular';
