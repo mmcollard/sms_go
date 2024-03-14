8ba78551b8da782578f338f39187f620596ca074
Yamisbel Orlianys Luna Sojo <ysojo@smsargentina.com.ar>
2024-03-14 16:07:19 -0300

    comienzo de barra lateral derecha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { BotonesComponent } from './layout/botones/botones.component';
 import { CardPublicacionComponent } from "./layout/card-publicacion/card-publicacion.component";
 import { BarraLateralComponent } from "./layout/barra-lateral/barra-lateral.component";
+import { BarraLateralDerechaComponent } from "./layout/barra-lateral-derecha/barra-lateral-derecha.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, BotonesComponent, BarraLateralComponent, CardPublicacionComponent]
+    imports: [RouterOutlet, BotonesComponent, BarraLateralComponent, CardPublicacionComponent, BarraLateralDerechaComponent]
 })
 export class AppComponent {
   title = 'proyectoangular';
